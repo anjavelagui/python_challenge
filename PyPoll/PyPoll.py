94e6c3f6d5e153646bbe989d373b2a2087a83244
anjavelagui <80436410+anjavelagui@users.noreply.github.com>
2021-04-05 15:52:28 -0400

    adding PyPoll.py

---
+++ PyPoll/PyPoll.py
@@ -0,0 +1,6 @@
+#os module allow to ceate a file paths across operating systems
+import os
+#Module for reading  csv files
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'election_data.csv')
